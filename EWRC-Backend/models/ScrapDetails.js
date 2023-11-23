5a2f77ede9c97ce2170975e6d30502b3f49086c4
Parshya <107461542+PKparshya@users.noreply.github.com>
2023-11-23 15:07:43 +0530

    Add files via upload
    EWRC Updated backend

---
+++ EWRC-Backend/models/ScrapDetails.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const ScrapSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    name:{
+        type: String,
+        required: true
+    },
+    phone:{
+        type: Number,
+        required: true
+    },
+    itemWeight: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    time: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    scrapDetails: {
+        type: String,
+        required: true
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+
+const ScrapDetails = mongoose.model('ScrapDetails', ScrapSchema);
+module.exports = ScrapDetails;
